99784a950a69da4dcbd5d0bcbbf724cf364ff7eb
root <root@localhost.localdomain>
2017-10-24 20:13:15 +0200

    Matrix Program

---
+++ tri.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+from random import randint
+import argparse
+
+
+class Some:
+	"""docstring for Some"""
+	def __init__(self, x,y):
+		self.x = x
+		self.y = y
+		self.matrix = [[randint(1,99) for i in range(self.y)] for a in range(self.x]
+	def gogo(self):
+		ce = -1
+		kiki = 0
+		print("Matrix: \n")
+		for line in self.matrix:
+			print("\tList[",kiki,"]","  ",end=" ")
+			ce += 1
+			kiki += 1
+			for go in range(len(line)):
+				if go > ce:
+					print("",end=" ")
+				else:
+					if line[go] in range(1,10):
+						print("[ ",line[go],"]",end=" ")
+					else:	
+						print("[",line[go],"]",end=" ")		
+			print()
+		print("\n")
+
+
+def Main():
+	argo = argparse.ArgumentParser("Enter two numbers")
+	argo.add_argument("-a",dest = "first",help = "Enter first number",type = int)
+	argo.add_argument("-b",dest = "sec",help = "Enter first number",type = int)
+	do = argo.parse_args()
+	soso = Some(do.first,do.sec)
+	soso.gogo()
+
+if __name__ == "__main__":
+	Main()	
